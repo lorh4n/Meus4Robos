3e4ecadcf9ba798bb2e0af28ec1ca8a410cb3a0c
Lorhan <53243389+Lorhan7@users.noreply.github.com>
2020-03-05 19:56:26 -0400

    Google Serach Images Implemented
    Co-Authored-By: lfelipep <lfelipep@users.noreply.github.com>

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function start() {
 
     const content = robots.state.load();
 
-    // console.dir(content, { depth: null });
+    console.dir(content, { depth: null });
 
 }
 
--- robots/image.js
+++ robots/image.js
@@ -3,17 +3,41 @@ const google = require('googleapis').google;
 const customSearch = google.customsearch('v1');
 
 const googleSearchCredentials = require('../credentials/google-credentials');
+
 async function robot() {
   const content = state.load();
 
-  const response = await customSearch.cse.list({
-    auth: googleSearchCredentials.apiKey,
-    cx: googleSearchCredentials.searchEngineId,
-    q: 'Michael Jackson',
-    searchType: 'image',
-    num: 1
-  });
-  console.dir(response, { depth: null });
-  process.exit(0);
+  await fetchImagesOfAllSentences(content);
+
+  state.save(content);
+
+  async function fetchImagesOfAllSentences(content) {
+    for (const sentence of content.sentences) {
+      const query = `${content.searchTerm} ${sentence.keywords[0]}`;
+      sentence.images = await fetchGoogleAndReturnImagesLinks(query);
+
+      sentence.googleSearchQuery = query;
+
+    }
+  }
+
+  async function fetchGoogleAndReturnImagesLinks(query) {
+    const response = await customSearch.cse.list({
+      auth: googleSearchCredentials.apiKey,
+      cx: googleSearchCredentials.searchEngineId,
+      q: query,
+      searchType: 'image',
+      num: 2
+    });
+
+    const imagesUrl = response.data.items.map((item) => {
+      return item.link;
+    });
+
+    return imagesUrl;
+  }
+
+
+
 }
 module.exports = robot;
